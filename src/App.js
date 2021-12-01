46ce3256cca9458490476dee22e46c6eae265ac2
VABIII <tbengtson82@gmail.com>
2021-11-30 20:39:06 -0600

    User info pulling from api and displaying in its own comp

--- src/App.js
+++ src/App.js
@@ -8,18 +8,58 @@ import User from "./components/User";
 
 class App extends React.Component {
 
+    state = {
+        user:
+            {
+                avatar: "",
+                followers: "",
+                repos: "",
+                name: "VABIII",
+                login: ",",
+                followerUrl: ""
+            },
+        followers: []
+    }
+
   componentDidMount() {
-    axios.get("https://api.github.com/users/VABIII")
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(err => {
-          console.error(err);
-        })
+    this.getPerson(this.state.user.name)
+    this.getFollowers(this.state.user.name)
   }
 
+  getPerson = name => {
+      axios.get(`https://api.github.com/users/${name}`)
+          .then(res => {
+              const data = res.data;
+              console.log(res.data);
+              this.setState({
+                  ...this.state,
+                  user:{
+                      avatar: data.avatar_url,
+                      followers: data.followers,
+                      repos: data.public_repos,
+                      name: data.name,
+                      login: data.login,
+                      followerUrl: data.followers_url
+                  }
+              })
+          })
+          .catch(err => {
+              console.error(err);
+          })
+  }
+
+    getFollowers = name => {
+        axios.get(`https://api.github.com/users/${name}/followers`)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
 
   render() {
+        console.log(this.state)
     return(
         <div className="search-container">
           <div className="header">
@@ -37,25 +77,24 @@ class App extends React.Component {
             </div>
           </div>
             <br/>
-        <div className="user">
-            <div className="user-img">
-                <img src="https://avatars.githubusercontent.com/u/87542373?v=4" alt="avatar"/>
-            </div>
-            <div className="user-info">
-               <h2>Vern</h2>
-               <p>(VABIII)</p>
-               <h4 id="repos">Total Repos:&nbsp;</h4>
-               <h4>Total Followers:&nbsp;</h4>
-            </div>
+            {/*<div className="user">*/}
+            {/*<div className="user-img">*/}
+            {/*    <img src="https://avatars.githubusercontent.com/u/87542373?v=4" alt="avatar"/>*/}
+            {/*</div>*/}
+            {/*<div className="user-info">*/}
+            {/*   <h2>{this.state.user.name}</h2>*/}
+            {/*   <p>({this.state.user.login})</p>*/}
+            {/*   <h4 id="repos">Total Repos:&nbsp;{this.state.user.repos}</h4>*/}
+            {/*   <h4>Total Followers:&nbsp;{this.state.user.followers}</h4>*/}
+            {/*</div>*/}
+            {/*</div>*/}
+            {/*<div className="followers">*/}
+            {/*    <div className="followers-header">*/}
+            {/*      <h3>Followers:</h3>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <User user={this.state.user}/>
         </div>
-        <div className="followers">
-            <div className="followers-header">
-              <h3>Followers:</h3>
-            </div>
-        </div>
-
-        </div>
-
     );
   }
 }
--- src/components/User.js
+++ src/components/User.js
@@ -8,8 +8,23 @@ class User extends React.Component {
 
     render() {
         return(
-            <div className="user">
-                Hi
+            <div>
+                <div className="user">
+                    <div className="user-img">
+                        <img src={`${this.props.user.avatar_url}`} alt="avatar"/>
+                    </div>
+                    <div className="user-info">
+                        <h2>{this.props.user.name}</h2>
+                        <p>({this.props.user.login})</p>
+                        <h4 id="repos">Total Repos:&nbsp;{this.props.user.repos}</h4>
+                        <h4>Total Followers:&nbsp;{this.props.user.followers}</h4>
+                    </div>
+                </div>
+                <div className="followers">
+                    <div className="followers-header">
+                        <h3>Followers:</h3>
+                    </div>
+                </div>
             </div>
         )
     }
